d1050c315d180ba4fdbb11a01502650f016d4b81
ben40d9 <benpaley6@gmail.com>
2022-08-17 00:53:26 -0700

    csv converted to an array of strings

---
+++ index.js
@@ -0,0 +1,8 @@
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const contents = readFileSync(join(__dirname, "nba_finals.csv"), {
+  encoding: "utf-8",
+});
+
+const data = contents.split("\r\n");
+console.log(data);
