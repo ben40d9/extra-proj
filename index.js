24e9969ca552e07f7989b6b69160684b07572938
ben40d9 <benpaley6@gmail.com>
2022-08-17 01:52:30 -0700

    played around w/ data. will get back to it tomorrow

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ const contents = readFileSync(join(__dirname, "nba_finals.csv"), {
   encoding: "utf-8",
 });
 
-const data = contents.split("\r\n");
-console.log(data);
+const csvFile = contents.split("\r\n");
+// console.log(csvFile);
+
+// //this creates one obj w ever csv val WRONG
+// const testArrOfObj = { ...data };
+// console.log(testArrOfObj);
+
+// const tryTheMap = data.map()
+
+//know i need to map through array ie, need 'eachString' to equal a single str in the arr:
+//data.map((eachString, index) => {(value: eachString, id: index + 1)})
