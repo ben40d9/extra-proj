c67cadb37d03c1736175db7bb1146f4c6d161d82
ben40d9 <benpaley6@gmail.com>
2022-08-18 12:19:36 -0700

    trouble here

---
+++ thisWillWork.js
@@ -0,0 +1,24 @@
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const contents = readFileSync(join(__dirname, "nba_finals.csv"), {
+  encoding: "utf-8",
+});
+// console.log(contents);
+
+const csvToArr = (str, seperator = ",") => {
+  const headers = str.slice(0, str.indexOf("\n")).split(seperator);
+  const rows = str.slice(str.indexOf("\n" + 1).split("\n"));
+
+  const arrofObjs = rows.map((row) => {
+    const values = row.split(seperator);
+    const objWithHeaders = headers.reduce((object, header, index) => {
+      object[header] = values[index];
+      return object;
+    }, {});
+    console.log(arrofObjs);
+    return objWithHeaders;
+  });
+  return arrofObjs;
+  //   console.log(arrofObjs);
+};
+console.log(csvToArr());
