2efdf83577328e4ad9db7d21d0667ebfdbae9b36
ben40d9 <benpaley6@gmail.com>
2022-08-18 16:50:09 -0700

    changed to outline working code that returns an arr of objs

--- anotherTestFile.js
+++ anotherTestFile.js
@@ -4,5 +4,44 @@ const contents = readFileSync(join(__dirname, "nba_finals.csv"), {
   encoding: "utf-8",
 });
 
-const csvFile = contents.split("\r\n");
-console.log(csvFile);
+const data = contents.split("\r\n");
+
+function solution(data) {
+  //create empty array
+  let arr = [];
+
+  //create header, data[0], using the first str in the array(const data)
+  //will use as the keys for every obj in arr.
+  //the headers comeback with their first letter capitalized
+  let uppercaseHeaders = data[0].split(",");
+  //changed my variable so that my keys could all be lowerCase
+  const headers = uppercaseHeaders.map((ele) => ele.toLowerCase());
+  //   console.log(headers);
+
+  //iterate over array and make each element in the array its own array
+  for (i = 0; i < data.length; i++) {
+    const eachLine = data[i].split(",");
+
+    //create empty obj
+    let obj = {};
+    //iterate through each array & push it to obj var we made
+    for (b = 0; b < eachLine.length; b++) {
+      obj[headers[b].trim()] = eachLine[b].trim();
+    }
+    arr.push(obj);
+  }
+  //   console.log(arr);
+
+  //   let found = arr.find((element) => element.year === "1950");
+  //   console.log(found);
+
+  //finding the winner by year
+  function getChampByYear(year) {
+    let found = arr.find((element) => {
+      console.log(element.year === year);
+    });
+  }
+  console.log(getChampByYear(1950));
+}
+
+solution(data);
