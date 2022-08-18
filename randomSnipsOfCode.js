1e2e3e3e72bc09d6d9af5801452288076e6eb3cd
ben40d9 <benpaley6@gmail.com>
2022-08-18 16:49:49 -0700

    random test

--- randomSnipsOfCode.js
+++ randomSnipsOfCode.js
@@ -5,3 +5,11 @@ for (let i of str) {
   arr[k++] = i;
 }
 console.log(arr);
+
+var obj = [
+  { name: "Max", age: 23 },
+  { name: "John", age: 20 },
+  { name: "Caley", age: 18 },
+];
+
+Object.keys(obj).forEach((key) => console.log(obj[key]));
