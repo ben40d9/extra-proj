06268fa9ab0321c27798e27ea0a53a71ab6ebd69
ben40d9 <benpaley6@gmail.com>
2022-08-18 15:39:27 -0700

    was showing thought process to joe in class and commented stuff out

--- test.js
+++ test.js
@@ -1,48 +1,54 @@
 const { readFileSync } = require("fs");
 const { join } = require("path");
+console.log(__dirname);
 const contents = readFileSync(join(__dirname, "nba_finals.csv"), {
   encoding: "utf-8",
 });
 
-const csvFile = contents.split("\r\n");
+const dataArr = contents.split("\r\n");
 // console.log(csvFile);
 
 // //this does not work because you cant split here b/c it is a : string[]
 // const arrOfArr = csvFile.split(",");
 // console.log(arrOfArr);
 
-const eachElement = csvFile.forEach((element) => {
-  //   console.log(element);
+// function solution(data) {
+//   for (i = 0; i ; i++ )
 
-  //this places everySingle str into an arr
-  const splitLineElement = element.split(",");
-  //   console.log(splitLineElement);
+// }
 
-  const arr = [splitLineElement];
-  //   console.log(arr);
+// const eachElement = csvFile.forEach((element) => {
+//   //   console.log(element);
 
-  //   splitLineElement = [splitLineElement];
-  //   console.log(splitLineElement);
+//   //this places everySingle str into an arr
+//   const splitLineElement = element.split(",");
+//   //   console.log(splitLineElement);
 
-  //this turns each 'arr' into an arr w/ an obj.
-  //I know this isn't the best way but I cant figure out how to loop through the string with an array "String[]"
-  const arrayObjects = arr.map((item) => {
-    return {
-      year: item[0],
-      champion: item[1],
-      runnerUp: item[2],
-      seriesRecord: item[3],
-      finalsMvp: item[4],
-    };
-  });
-  console.log(arrayObjects);
+//   const arr = [splitLineElement];
+//   //   console.log(arr);
 
-  //     //this doesn't work yet, just messing w/ it
-  //     function findChampionByYear(year) {
-  //       return (objOfInputedYear = arrayObjects.find((obj) => obj.year === year));
-  //     }
-  //     console.log(findChampionByYear(2011));
-});
+//   //   splitLineElement = [splitLineElement];
+//   //   console.log(splitLineElement);
+
+//   //this turns each 'arr' into an arr w/ an obj.
+//   //I know this isn't the best way but I cant figure out how to loop through the string with an array "String[]"
+//   const arrayObjects = arr.map((item) => {
+//     return {
+//       year: item[0],
+//       champion: item[1],
+//       runnerUp: item[2],
+//       seriesRecord: item[3],
+//       finalsMvp: item[4],
+//     };
+//   });
+//   console.log(arrayObjects);
+
+//   //     //this doesn't work yet, just messing w/ it
+//   //     function findChampionByYear(year) {
+//   //       return (objOfInputedYear = arrayObjects.find((obj) => obj.year === year));
+//   //     }
+//   //     console.log(findChampionByYear(2011));
+// });
 
 // //just test shit
 // const arrOfArrs = () => {
