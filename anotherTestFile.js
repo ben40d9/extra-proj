1b82ffd6211377a9548cb7a931902632a26c6183
ben40d9 <benpaley6@gmail.com>
2022-08-22 09:46:06 -0700

    this is the test file

--- anotherTestFile.js
+++ anotherTestFile.js
@@ -37,46 +37,54 @@ const arrNoKeyObj = arrOfObjs.filter((obj) => obj.year !== "year" || "");
 //W/O RANDOM SHIT EVERYWHERE IN THE LOG
 
 //function to get name of champion by entering the year
-const getChampByYear = (year) => {
-  const found = arrOfObjs.find((obj) => obj.year === `${year}`);
-  //   console.log(found);
-  return console.log(found.winner);
+// const getChampByYear = (year) => {
+//   const found = arrOfObjs.find((obj) => obj.year === `${year}`);
+//   //   console.log(found);
+//   return console.log(found.winner);
+// };
+// getChampByYear(2010);
+
+// //function to find the amount of times 'x' team has won the championiship
+// function findChampionshipsWon(arr, key) {
+//   let arr2 = [];
+
+//   arr.forEach((x) => {
+//     // looking to see if any object in arr2,
+//     //contains the key value(has the same winner value)
+//     if (
+//       arr2.some((val) => {
+//         return val[key] == x[key];
+//       })
+//     ) {
+//       //then increase the occurrence by 1
+//       arr2.forEach((k) => {
+//         if (k[key] === x[key]) {
+//           //ASK WHY THE SPACE PUTS KEY INTO A STRING
+//           // k["times Won"]++;
+//           k["timesWon"]++;
+//         }
+//       });
+//     } else {
+//       // then create a new object, initialize it with the current
+//       //iteration key value and set the timesWon to 1
+//       let a = {};
+//       a[key] = x[key];
+//       // a["times Won"] = 1;
+//       a["timesWon"] = 1;
+//       arr2.push(a);
+//     }
+//   });
+
+//   return arr2;
+// }
+// let arr = arrNoKeyObj;
+// let key = "winner";
+// console.log(findChampionshipsWon(arr, key));
+
+//function to get name of finalsMvp in specific year
+const mvpByYear = (year) => {
+  const found = arrNoKeyObj.find((obj) => obj.year === `${year}`);
+  console.log(found);
 };
-getChampByYear(2010);
 
-//function to find the amount of times 'x' team has won the championiship
-function findChampionshipsWon(arr, key) {
-  let arr2 = [];
-
-  arr.forEach((x) => {
-    // looking to see if any object in arr2,
-    //contains the key value(has the same winner value)
-    if (
-      arr2.some((val) => {
-        return val[key] == x[key];
-      })
-    ) {
-      //then increase the occurrence by 1
-      arr2.forEach((k) => {
-        if (k[key] === x[key]) {
-          //ASK WHY THE SPACE PUTS KEY INTO A STRING
-          // k["times Won"]++;
-          k["timesWon"]++;
-        }
-      });
-    } else {
-      // then create a new object, initialize it with the current
-      //iteration key value and set the timesWon to 1
-      let a = {};
-      a[key] = x[key];
-      // a["times Won"] = 1;
-      a["timesWon"] = 1;
-      arr2.push(a);
-    }
-  });
-
-  return arr2;
-}
-let arr = arrNoKeyObj;
-let key = "winner";
-console.log(findChampionshipsWon(arr, key));
+console.log(mvpByYear(2010));
