870d14836d1f6c0759c3dbf3c950da863c7d3677
ben40d9 <benpaley6@gmail.com>
2022-08-22 05:33:14 -0700

    created new function to iterate over arr and return the team listed w/ the # of championships they have

--- anotherTestFile.js
+++ anotherTestFile.js
@@ -27,18 +27,66 @@ for (i = 0; i < data.length; i++) {
   arrOfObjs.push(obj);
 }
 //now the var arr is an arrayOfObjects
-console.log(arrOfObjs);
+// console.log(arrOfObjs);
+
+//takes off the header obj in the arrOfObjs
+const arrNoKeyObj = arrOfObjs.filter((obj) => obj.year !== "year" || "");
+// console.log(arrNoKeyObj);
 
 //THIS FILE IS FOR TESTING INDIVIDUAL FUNCTIONS IN THE LOG
 //W/O RANDOM SHIT EVERYWHERE IN THE LOG
 
-const getYearsofChampionships = (team) => {
-  console.log(
-    arrOfObjs.forEach((obj) => {
-      console.log(Object.values(obj));
-    })
-  );
-
-  //   const found = arrOfObjs.filter();
+//function to get name of champion by entering the year
+const getChampByYear = (year) => {
+  const found = arrOfObjs.find((obj) => obj.year === `${year}`);
+  //   console.log(found);
+  return console.log(found.winner);
 };
-getYearsofChampionships();
+getChampByYear(2010);
+
+// const getYearsofChampionships = (team) => {
+//   arrOfObjs.filter((obj) => {
+//     // const upTeam = team.toUpperCase();
+//     if (obj.winner === `${team}`) {
+//       console.log(team.winner);
+//       return obj.year;
+//     }
+//   });
+// };
+// getYearsofChampionships();
+
+// function findOcc(arr, key) {
+//   let arr2 = [];
+
+//   arr.forEach((x) => {
+//     // Checking if there is any object in arr2
+//     // which contains the key value
+//     if (
+//       arr2.some((val) => {
+//         return val[key] == x[key];
+//       })
+//     ) {
+//       // If yes! then increase the occurrence by 1
+//       arr2.forEach((k) => {
+//         if (k[key] === x[key]) {
+//           // k["times Won"]++;
+//           k["timesWon"]++;
+//         }
+//       });
+//     } else {
+//       // If not! Then create a new object initialize
+//       // it with the present iteration key's value and
+//       // set the occurrence to 1
+//       let a = {};
+//       a[key] = x[key];
+//       // a["times Won"] = 1;
+//       a["timesWon"] = 1;
+//       arr2.push(a);
+//     }
+//   });
+
+//   return arr2;
+// }
+// let arr = arrOfObjs;
+// let key = "winner";
+// console.log(findOcc(arr, key));
