81700f618bb22fad16fa3e43f94e837c69c8ca52
ben40d9 <benpaley6@gmail.com>
2022-08-23 22:21:54 -0700

    created reduce method that finds mvps, then lists them in a list w/ how many mvps they have

--- randomSnipsOfCode.js
+++ randomSnipsOfCode.js
@@ -1,43 +1,46 @@
-// const str = "Kamran";
-// let arr = [];
-// let k = 0;
-// for (let i of str) {
-//   arr[k++] = i;
-// }
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const contents = readFileSync(join(__dirname, "nba_finals.csv"), {
+  encoding: "utf-8",
+});
+
+const data = contents.split("\r\n");
+
+let arrOfObjs = [];
+
+let uppercaseHeaders = data[0].split(",");
+
+const headers = uppercaseHeaders.map((ele) => ele.toLowerCase());
+
+for (i = 0; i < data.length; i++) {
+  const eachLine = data[i].split(",");
+
+  //create empty obj
+  let obj = {};
+  //iterate through each array & push it to obj var we made
+  for (b = 0; b < eachLine.length; b++) {
+    obj[headers[b].trim()] = eachLine[b].trim().toLowerCase();
+  }
+  arrOfObjs.push(obj);
+}
+//now the var arr is an arrayOfObjects
+
+//takes off the header obj in the arrOfObjs
+let arr = arrOfObjs.filter((obj) => obj.winner !== "winner" || null);
 // console.log(arr);
 
-// var obj = [
-//   { name: "Max", age: 23 },
-//   { name: "John", age: 20 },
-//   { name: "Caley", age: 18 },
-// ];
+const reduceResult = arr.reduce((acc, obj) => {
+  return { ...acc, [obj.mvp]: (acc[obj.mvp] || 0) + 1 };
+}, {});
+console.log(reduceResult);
 
-// Object.keys(obj).forEach((key) => console.log(obj[key]));
-
-let students = [
-  { id: "001", name: "Anish", sports: "Cricket" },
-  { id: "002", name: "Smriti", sports: "Basketball" },
-  { id: "003", name: "Rahul", sports: "Cricket" },
-  { id: "004", name: "Bakul", sports: "Basketball" },
-  { id: "005", name: "Nikita", sports: "Hockey" },
+const orders = [
+  { id: "1", status: "pending" },
+  { id: "2", status: "pending" },
+  { id: "3", status: "pending" },
+  { id: "4", status: "pending" },
 ];
 
-/* Applying filter function on students array to
- retrieve those students Objects who play 
- basketball and then the new array returned by
- filter method is mapped in order to get the name
- of basketball players instead of whole object.*/
-let basketballPlayers = students
-  .filter(function (student) {
-    return student.sports === "Basketball";
-  })
-  .map(function (student) {
-    return student.name;
-  });
-
-console.log("Basketball Players are:");
-
-// Printing out the name of Basketball players
-basketballPlayers.forEach(function (players) {
-  console.log(players);
-});
+// result = orders.reduce((acc, order) => {
+//   return
+// })
