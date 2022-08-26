910264cf0033b197e6146603451b4b3d6cb2cb39
ben40d9 <benpaley6@gmail.com>
2022-08-26 00:18:28 -0700

    changed parser function to be able to parse through obj and get the file path. just not exactly sure what you want to be returned scope is skewed, will test if joe is not awake

--- src/parse-datasets/parseFile.js
+++ src/parse-datasets/parseFile.js
@@ -2,23 +2,27 @@
 // the relvant parser from ./parsers/ if there is a match. otherwise (default case) it will return a soft error.
 
 const parsers = require("./parsers/index");
-console.log(parsers);
+//logs to see how I can manipulate data
+// console.log(parsers);
+// console.log(Object.keys(parsers));
 
-// function parseFile(path) {
-//   // get the extension off the file path
-//   const splitPath = path.split(".");
-//   const filetype = splitPath[splitPath.length - 1];
+function parseFile(path) {
+  //get the extension off of the file path
+  const splitPath = path.split(".");
+  const fileType = splitPath[splitPath.length - 1];
+  // console.log(fileType);
 
-//   // find a parser whose 'ext' field matches the extension
-//   const matchedParser = parsers.find((p) => p.ext === filetype);
-//   if (matchedParser !== undefined) {
-//     matchedParser.parse(path);
-//     return;
-//   }
-
-//   // default behavior
-//   console.log(`\nNo parser found for the given filetype: ${filetype}`);
-//   return;
-// }
-
-// parseFile("../../datasets/nba_finals.csv ");
+  // find a parser whose 'ext' field matches the extension
+  // const keyOfPath = Object.keys();
+  for (const key in parsers) {
+    // console.log(key);
+    if (key === fileType) {
+      const keyOfFound = key;
+      // console.log(keyOfFound);
+    }
+  }
+  //default behavior
+  console.log(`\nNo parser found for the given filetype: ${fileType}`);
+  return;
+}
+parseFile("../../datasets/nba_finals.xyz");
