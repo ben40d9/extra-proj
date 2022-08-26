9d1ba94022fc7040c1f66794bfabdf5371d99a77
Joe <joe@rconjoe.com>
2022-08-26 13:45:43 -0700

    test scaffold

---
+++ src/normalizers/filepath.test.js
@@ -0,0 +1,9 @@
+const { buildAbsoluteTargetPath } = require('./filepath');
+
+test('buildAbsoluteTargetPath returns an absolute path', () => {
+  // 1. ARRANGE (set up) your mock data
+
+  // 2. ACT - run your function with applicable mock data
+
+  // 3. ASSERT - use jest expect() and matchers to confirm function responds as it should.
+})
