504787b015de4220a9d54831a58f22cc0d4e020b
ben40d9 <benpaley6@gmail.com>
2022-08-29 22:08:45 -0700

    commented out code so jest wasnt running tests for them, old trash code. will delete soon.

--- dump/thisWillWork.js
+++ dump/thisWillWork.js
@@ -1,25 +1,25 @@
-const { readFileSync } = require("fs");
-const { join } = require("path");
-const contents = readFileSync(join(__dirname, "nba_finals.csv"), {
-  encoding: "utf-8",
-});
-// console.log(contents);
-const data = contents.split("\r\n");
+// const { readFileSync } = require("fs");
+// const { join } = require("path");
+// const contents = readFileSync(join(__dirname, "nba_finals.csv"), {
+//   encoding: "utf-8",
+// });
+// // console.log(contents);
+// const data = contents.split("\r\n");
 
-const csvToArr = (str, seperator = ",") => {
-  const headers = str.slice(0, str.indexOf("\n")).split(seperator);
-  const rows = str.slice(str.indexOf("\n" + 1).split("\n"));
+// const csvToArr = (str, seperator = ",") => {
+//   const headers = str.slice(0, str.indexOf("\n")).split(seperator);
+//   const rows = str.slice(str.indexOf("\n" + 1).split("\n"));
 
-  const arrofObjs = rows.map((row) => {
-    const values = row.split(seperator);
-    const objWithHeaders = headers.reduce((object, header, index) => {
-      object[header] = values[index];
-      return object;
-    }, {});
-    console.log(arrofObjs);
-    return objWithHeaders;
-  });
-  return arrofObjs;
-  //   console.log(arrofObjs);
-};
-console.log(csvToArr());
+//   const arrofObjs = rows.map((row) => {
+//     const values = row.split(seperator);
+//     const objWithHeaders = headers.reduce((object, header, index) => {
+//       object[header] = values[index];
+//       return object;
+//     }, {});
+//     console.log(arrofObjs);
+//     return objWithHeaders;
+//   });
+//   return arrofObjs;
+//   //   console.log(arrofObjs);
+// };
+// console.log(csvToArr());
