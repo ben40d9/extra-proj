21d591676325a36a3dd37f0cd1461030417b9b33
Joe <joe@rconjoe.com>
2022-08-31 16:08:50 -0700

    init prompts folder- this should hold question objects that we can pass to prompt()

---
+++ src/inquirer/prompts/askEmail.js
@@ -0,0 +1,8 @@
+// describe the question prompt object we will use to ask the user their email.
+// in some other file, we will import "prompt.js" as well as this question, 
+// and feed one into the other.
+
+const askEmail = {
+  // https://github.com/SBoudrias/Inquirer.js#objects
+  // there should be an input question object here.
+}
