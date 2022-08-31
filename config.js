1ab6a5f6835f8aeab47dbe0919ec6c135df32848
Joe <joe@rconjoe.com>
2022-08-31 16:09:52 -0700

    clean this up

--- config.js
+++ config.js
@@ -1,26 +1,4 @@
 const { convict } = require('convict');
-const options = {
-  ...userOptions,
-}
 
-const buildOptions = userOptions => convict({
-
-});
-
-dev: {
-  // logger options. see https://github.com/pinojs/pino/blob/master/docs/api.md#options
-  logger: {
-    transport: {
-        ...userTransport,
-        target: 'pino-pretty',
-      },
-  }
-},
-prod: {
-
-}
-module.exports = {
-  buildOptions,
-}
-
-
+// define config schema here.
+// https://github.com/mozilla/node-convict/tree/master/packages/convict#usage
