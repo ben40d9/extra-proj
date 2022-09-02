90bd5a3e3d96b54ad5faf9345ad0079bd263bc4e
ben40d9 <benpaley6@gmail.com>
2022-09-02 14:32:35 -0700

    set up config outline

--- config.js
+++ config.js
@@ -7,43 +7,14 @@ const { convict } = require("convict");
 //benTry
 //---
 // have to npm install convict-format-with-validator
-// var config = convict({
-//   env: {
-//     doc: "The application environment.",
-//     format: ["production", "development", "test"],
-//     default: "development",
-//     env: "NODE_ENV",
-//   },
-//   ip: {
-//     doc: "The IP address to bind.",
-//     format: "ipaddress",
-//     default: "127.0.0.1",
-//     env: "IP_ADDRESS",
-//   },
-//   port: {
-//     doc: "The port to bind.",
-//     format: "port",
-//     default: 8080,
-//     env: "PORT",
-//     arg: "port",
-//   },
-// });
-
-// var config = convict({
-//   db: {
-//     name: {
-//       format: String,
-//       default: "",
-//     },
-//     synchro: {
-//       active: {
-//         format: "Boolean",
-//         default: false,
-//       },
-//       remote_url: {
-//         format: "url",
-//         default: "http://localhost:8080/",
-//       },
-//     },
-//   },
-// });
+var config = convict({
+  env: {
+    doc: "The application environment.",
+    format: ["production", "development", "test"],
+    default: "development",
+    env: "NODE_ENV",
+  },
+  gh: {
+    email: {},
+  },
+});
